8458a7da8632718b8c31ae3e7fcc7d90451122cd
Ezequiel <crespo.ep@gmail.com>
2022-08-02 16:29:05 -0300

    tests refactoring: using beforeEach for addItem methods

--- smartcontracts/test/NFTMarketplace.test.js
+++ smartcontracts/test/NFTMarketplace.test.js
@@ -7,7 +7,9 @@ const IERC165 = require('../artifacts/contracts/NFT.sol/NFT.json');
 
 describe("NFT marketplace", async () => {
   const ITEM_PRICE_EXAMPLE = ethers.utils.parseEther("1");
-  const ITEM_ID_EXAMPLE = BigNumber.from("1")
+
+  const FIRST_ITEM_ID = BigNumber.from("1");
+  const SECOND_ITEM_ID = BigNumber.from("2");
 
   let
     deployer,
@@ -49,10 +51,10 @@ describe("NFT marketplace", async () => {
   describe("adding an item", async () => {
     it('should be reverted if item price is not greater than zero', async () => {
       await expect(
-        marketplaceContract.connect(user1).addItem(itemMock.address, ITEM_ID_EXAMPLE, 0)
+        marketplaceContract.connect(user1).addItem(itemMock.address, FIRST_ITEM_ID, 0)
       ).to.be.revertedWith("PriceMustBeGreaterThanZero()");
       await expect(
-        marketplaceContract.connect(user1).addItem(itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE)
+        marketplaceContract.connect(user1).addItem(itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE)
       ).not.to.be.revertedWith("PriceMustBeGreaterThanZero()");
     });
 
@@ -60,22 +62,22 @@ describe("NFT marketplace", async () => {
       await itemMock.mock.supportsInterface.returns(false);
 
       await expect(
-        marketplaceContract.connect(user1).addItem(itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE)
+        marketplaceContract.connect(user1).addItem(itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE)
       ).to.be.revertedWith("ProvidedAddressDoesNotSupportERC721Interface()")
     });
 
     it('should be reverted if nft was already added in the marketplace', async () => {
-      await marketplaceContract.connect(user1).addItem(itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE);
+      await marketplaceContract.connect(user1).addItem(itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE);
       await expect(
-        marketplaceContract.connect(user1).addItem(itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE)
+        marketplaceContract.connect(user1).addItem(itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE)
       ).to.be.revertedWith("ItemAlreadyExistsInTheMarketplace()")
     });
 
     it('should add the new item to the listing successfully', async () => {
       await expect(marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE)
+        itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE)
       ).to.emit(marketplaceContract, "ItemAdded")
-        .withArgs(user1.address, itemMock.address, ITEM_PRICE_EXAMPLE, ITEM_ID_EXAMPLE)
+        .withArgs(user1.address, itemMock.address, ITEM_PRICE_EXAMPLE, FIRST_ITEM_ID)
 
       const nft = await marketplaceContract.itemByAddressAndId(itemMock.address, 1)
 
@@ -85,124 +87,121 @@ describe("NFT marketplace", async () => {
   })
 
   describe("removing an item", async () => {
+    beforeEach(async () => {
+      await marketplaceContract.connect(user1).addItem(
+        itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE
+      )
+    })
+
     it('should fail if item does not exist', async () => {
       await expect(marketplaceContract.removeItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE
+        SECOND_ITEM_ID
       )).to.be.revertedWith("ItemIsNotListedInTheMarketplace()")
     });
 
     it('should remove the item from listing', async () => {
-      await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
-      )
-
       await expect(marketplaceContract.removeItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE
-      )).to.emit(marketplaceContract, "ItemRemoved").withArgs(itemMock.address, ITEM_ID_EXAMPLE);
+        FIRST_ITEM_ID
+      )).to.emit(marketplaceContract, "ItemRemoved").withArgs(itemMock.address, FIRST_ITEM_ID);
 
-      const item = await marketplaceContract.itemByAddressAndId(itemMock.address, ITEM_ID_EXAMPLE)
+      const item = await marketplaceContract.itemByAddressAndId(itemMock.address, FIRST_ITEM_ID)
       expect(item.price).to.equal(0)
     });
   })
 
   describe("updating an item", async () => {
+    beforeEach(async () => {
+      await marketplaceContract.connect(user1).addItem(
+        itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE
+      )
+    })
+
     it('should fail if item does not exist', async () => {
       await expect(marketplaceContract.updateItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE,
+        SECOND_ITEM_ID,
         ITEM_PRICE_EXAMPLE
       )).to.be.revertedWith("ItemIsNotListedInTheMarketplace()")
     });
 
     it('should fail if new price is not greater than zero', async () => {
-      await marketplaceContract.connect(user1).addItem(itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE)
       const newPrice = 0;
       await expect(
-        marketplaceContract.updateItem(itemMock.address, ITEM_ID_EXAMPLE, newPrice)
+        marketplaceContract.updateItem(itemMock.address, FIRST_ITEM_ID, newPrice)
       ).to.be.revertedWith("PriceMustBeGreaterThanZero()");
     });
 
     it('should change item price correctly', async () => {
-      await marketplaceContract.connect(user1).addItem(itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE)
       const newPrice = ITEM_PRICE_EXAMPLE.mul(2);
 
       await expect(
-        marketplaceContract.updateItem(itemMock.address, ITEM_ID_EXAMPLE, newPrice)
+        marketplaceContract.updateItem(itemMock.address, FIRST_ITEM_ID, newPrice)
       ).to
         .emit(marketplaceContract, "ItemUpdated")
         .withArgs(
           itemMock.address,
-          ITEM_ID_EXAMPLE,
+          FIRST_ITEM_ID,
           newPrice
         );
 
-      const item = await marketplaceContract.itemByAddressAndId(itemMock.address, ITEM_ID_EXAMPLE)
+      const item = await marketplaceContract.itemByAddressAndId(itemMock.address, FIRST_ITEM_ID)
       expect(item.price).to.equal(newPrice)
     });
   })
 
   describe("buy an item", async () => {
+    beforeEach(async () => {
+      await marketplaceContract.connect(user1).addItem(
+        itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE
+      )
+    })
+
     it('should fail if items does not exist', async () => {
       await expect(marketplaceContract.connect(user1).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ITEM_PRICE_EXAMPLE }
+        SECOND_ITEM_ID, { value: ITEM_PRICE_EXAMPLE }
       )).to.be.revertedWith("ItemIsNotListedInTheMarketplace()")
     });
 
     it('should fail if buyer is the same as seller', async () => {
-      await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
-      )
       await expect(marketplaceContract.connect(user1).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ethers.utils.parseEther("1") }
+        FIRST_ITEM_ID, { value: ethers.utils.parseEther("1") }
       )).to.be.revertedWith("SellerCannotBuyItsOwnItem()")
     });
 
     it('should fail if payment is not exactly equal to item price', async () => {
-      await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
-      )
-
       await expect(marketplaceContract.connect(user2).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ethers.utils.parseEther("0.9") }
+        FIRST_ITEM_ID, { value: ethers.utils.parseEther("0.9") }
       )).to.be.revertedWith("PaymentIsNotExact()")
 
       await expect(marketplaceContract.connect(user2).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ethers.utils.parseEther("1.1") }
+        FIRST_ITEM_ID, { value: ethers.utils.parseEther("1.1") }
       )).to.be.revertedWith("PaymentIsNotExact()")
     });
 
     it.skip('should transfer ownership to buyer', async () => {});
 
     it('should remove item from the listing after operation is done', async () => {
-      await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
-      )
-
       await expect(marketplaceContract.connect(user2).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ITEM_PRICE_EXAMPLE }
+        FIRST_ITEM_ID, { value: ITEM_PRICE_EXAMPLE }
       ))
 
-      const item = await marketplaceContract.itemByAddressAndId(itemMock.address, ITEM_ID_EXAMPLE);
+      const item = await marketplaceContract.itemByAddressAndId(itemMock.address, FIRST_ITEM_ID);
 
       expect(item.seller).to.equal(ethers.constants.AddressZero)
       expect(item.price).to.equal(BigNumber.from("0"))
     });
 
     it('should emit the ItemBought event', async () => {
-      await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
-      )
-
       await expect(marketplaceContract.connect(user2).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ITEM_PRICE_EXAMPLE }
+        FIRST_ITEM_ID, { value: ITEM_PRICE_EXAMPLE }
       )).to.emit(marketplaceContract, "ItemBought")
         .withArgs(user1.address, ITEM_PRICE_EXAMPLE, user2.address)
     });
@@ -213,14 +212,16 @@ describe("NFT marketplace", async () => {
     const platformPayment = platformFee.mul(ITEM_PRICE_EXAMPLE).div(1e2);
     const sellerPayment = ITEM_PRICE_EXAMPLE.sub(platformPayment);
 
-    it('should be done successfully', async () => {
+    beforeEach(async () => {
       await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
+        itemMock.address, FIRST_ITEM_ID, ITEM_PRICE_EXAMPLE
       )
+    })
 
+    it('should be done successfully', async () => {
       await expect(marketplaceContract.connect(user2).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ITEM_PRICE_EXAMPLE }
+        FIRST_ITEM_ID, { value: ITEM_PRICE_EXAMPLE }
       ))
 
       await expect(
@@ -229,13 +230,9 @@ describe("NFT marketplace", async () => {
     });
 
     it("should send platform fee to the owner", async () => {
-      await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
-      )
-
       await expect(marketplaceContract.connect(user2).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ITEM_PRICE_EXAMPLE }
+        FIRST_ITEM_ID, { value: ITEM_PRICE_EXAMPLE }
       ))
 
       expect(
@@ -244,13 +241,9 @@ describe("NFT marketplace", async () => {
     });
 
     it('should fail if trying to withdraw twice in a row', async () => {
-      await marketplaceContract.connect(user1).addItem(
-        itemMock.address, ITEM_ID_EXAMPLE, ITEM_PRICE_EXAMPLE
-      )
-
       await expect(marketplaceContract.connect(user2).buyItem(
         itemMock.address,
-        ITEM_ID_EXAMPLE, { value: ITEM_PRICE_EXAMPLE }
+        FIRST_ITEM_ID, { value: ITEM_PRICE_EXAMPLE }
       ))
 
       await expect(
