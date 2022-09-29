6cc98c982e9ecca29c785631b935b974ef7801b4
Ezequiel <crespo.ep@gmail.com>
2022-09-29 14:20:20 -0300

    .env.example file and hardhat.config modifications

--- smartcontracts/hardhat.config.ts
+++ smartcontracts/hardhat.config.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import { HardhatUserConfig } from "hardhat/config";
 
 // Disabled until mocking be developed, using mocking from hardhat-waffle
@@ -16,9 +17,27 @@ const config: HardhatUserConfig = {
       chainId: 31337,
     }
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: !!(process.env.REPORT_GAS)
+  },
   namedAccounts: {
     deployer: {
       default: 0,
+    },
+    user1: {
+      default: 1,
+      4: 1
+    },
+    user2: {
+      default: 2,
+      4: 2
+    },
+    user3: {
+      default: 3,
+      4: 3
     }
   }
 };
